08c5dfaab20ca791f88159d9263a7f66d6fcb507
vinayak1514 <75493987+vinayak1514@users.noreply.github.com>
2024-03-07 14:13:34 +0530

    Add files via upload

---
+++ sumOfArray.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int getsum(int *arr,int size){
+    if(size==0){
+        return 0;
+    }
+    if(size==1){
+        return arr[0];
+    }
+
+    int remainingPart = getsum(arr+1,size-1);
+    int sum  = arr[0] + remainingPart;
+    return sum;
+}
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    int size = 5;
+
+    int sum = getsum(arr,size);
+
+    cout << "sum is "<<sum;
+
+        return 0;
+}
